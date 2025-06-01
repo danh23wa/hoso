9b142b824abc3d1fb3bc3fb5435f0b19a5131768
po-danh1 <nguyendanh2147@gmail.com>
2025-06-01 22:56:10 +0700

    fix2

--- my-app/src/component/home/home.tsx
+++ my-app/src/component/home/home.tsx
@@ -1,4 +1,3 @@
-
 import { motion } from 'framer-motion';
 import "bootstrap/dist/css/bootstrap.min.css";
 import profilePic from "../../../public/tsconfig.app.svg"; // Đường dẫn đến hình ảnh
@@ -7,43 +6,40 @@ import { useInView } from 'react-intersection-observer';
 import "./home.css";
 
 const Home = () => {
-  const { ref, inView } = useInView({
+  const { ref } = useInView({
     threshold: 0.1,
     triggerOnce: false,
   });
-  
 
   return (
     <div id="Home" className="d-flex flex-row" style={{ height: '100vh' }}>
-  
       <motion.div ref={ref} className="profile-container w-50 d-flex justify-content-center align-items-center">
-  <img 
-    src={profilePic} 
-    alt="Profile" 
-    className="img-fluid rounded-circle" 
-    style={{ width: '250px', height: '250px', objectFit: 'cover' }} 
-  />
-</motion.div>
+        <img
+          src={profilePic}
+          alt="Profile"
+          className="img-fluid rounded-circle"
+          style={{ width: '250px', height: '250px', objectFit: 'cover' }}
+        />
+      </motion.div>
 
-<motion.div ref={ref} className="text-container w-50 d-flex flex-column justify-content-center align-items-start p-4">
-  <h1 className="display-4">Hi,</h1>
-  <h2>I'm Nguyễn Qúi Danh</h2>
-  <p>Full Stack Software Developer</p>
-  <a href="#Contact" className="btn btn-primary">Contact</a>
-  
-  <div>
-    <h4>
-      <a href="https://github.com/danh23wa" target="_blank" rel="noopener noreferrer" className="icon">
-        <FaGithub size={24} />
-      </a>
-      {" | "}
-      <a href="mailto:nguyendanh2147@gmail.com" className="icon">
-        <FaEnvelope size={24} />
-      </a>
-    </h4>
-  </div>
-</motion.div>
+      <motion.div ref={ref} className="text-container w-50 d-flex flex-column justify-content-center align-items-start p-4">
+        <h1 className="display-4">Hi,</h1>
+        <h2>I'm Nguyễn Quí Danh</h2>
+        <p>Full Stack Software Developer</p>
+        <a href="#Contact" className="btn btn-primary">Contact</a>
 
+        <div>
+          <h4>
+            <a href="https://github.com/danh23wa" target="_blank" rel="noopener noreferrer" className="icon">
+              <FaGithub size={24} />
+            </a>
+            {" | "}
+            <a href="mailto:nguyendanh2147@gmail.com" className="icon">
+              <FaEnvelope size={24} />
+            </a>
+          </h4>
+        </div>
+      </motion.div>
     </div>
   );
 };
